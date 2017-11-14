070081940aa4072173433c20538fcf593089b1be
bonnie chang <b22379222@gmail.com>
2017-11-14 10:10:22 +0800

    Guess

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -2,19 +2,43 @@ package com.example.user.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
+import static com.example.user.guess100.R.id.number;
+
 public class MainActivity extends AppCompatActivity {
+    Random random = new Random();
+    int ram = random.nextInt(100) + 1;
+    int max = 100;
+    int min = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-     EditText num = (EditText) findViewById(R.id.number);
-     TextView sec = (TextView) findViewById(R.id.secret);
-     TextView info = (TextView) findViewById(R.id.info);
-     Button send = (Button) findViewById(R.id.send);
+
+    public void guess100(View view) {
+        TextView sec = (TextView) findViewById(R.id.secret);
+        sec.setText(Integer.toString(ram));
+        EditText edNumber = (EditText) findViewById(number);
+        TextView s = (TextView) findViewById(R.id.secret);
+        TextView i = (TextView) findViewById(R.id.info);
+        Button b = (Button) findViewById(R.id.send);
+        int num = Integer.parseInt(edNumber.getText().toString());
+        if (num > ram) {
+            i.setText(min + " to " + num);
+            max = num;
+        } else if (ram > num) {
+            i.setText(num + " to " + max);
+            min = num;
+        } else {
+            i.setText(num + "bomb!!");
+        }
+    }
 }
